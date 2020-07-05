10d6f9684653d0a224b8bc92a42d309ead0a3d3e
Andreas Giebel <andreas.giebel1998@hotmail.de>
2020-07-05 02:35:45 +0200

    Update Timer.java

--- src/de/andi/timer/Timer.java
+++ src/de/andi/timer/Timer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 by <name>
+ * Copyright Â© 2015 by Tryonex
  * This file is part of Timer
  * Timer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
